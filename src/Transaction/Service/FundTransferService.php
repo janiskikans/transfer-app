6ffd009732285823bb0d9cb3981d568c9c40850f
Janis Kikans <janis.kikans@gmail.com>
2025-06-08 18:14:45 +0300

    add locks

--- src/Transaction/Service/FundTransferService.php
+++ src/Transaction/Service/FundTransferService.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace App\Transaction\Service;
 
+use App\Account\Entity\Account;
 use App\Currency\Enum\Currency as CurrencyEnum;
 use App\Currency\Exception\CurrencyRateNotFoundException;
 use App\Currency\Service\CurrencyConversionService;
@@ -13,6 +14,7 @@ use App\Transaction\Exception\TransferFailedException;
 use App\Transaction\Factory\TransactionFactory;
 use Doctrine\DBAL\Exception;
 use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\Lock\LockFactory;
 use Throwable;
 
 readonly class FundTransferService
@@ -22,6 +24,7 @@ readonly class FundTransferService
         private CurrencyConversionService $conversionService,
         private EntityManagerInterface $em, // TODO: Repo?,
         private TransactionFactory $transactionFactory,
+        private LockFactory $lockFactory,
     ) {
     }
 
@@ -38,7 +41,10 @@ readonly class FundTransferService
         $debitAmount = $this->convertRequestAmountToCurrency($transferRequest, $sender->getCurrency()->toEnum());
         $creditAmount = $this->convertRequestAmountToCurrency($transferRequest, $recipient->getCurrency()->toEnum());
 
-        // TODO: Mutex
+        $lock = $this->lockFactory->createLock($this->getLockKey($sender, $recipient), 5);
+        if (!$lock->acquire()) {
+            throw new TransferFailedException('Fund transfer failed. Another transfer is in progress.');
+        }
 
         $this->em->getConnection()->beginTransaction();
 
@@ -65,6 +71,8 @@ readonly class FundTransferService
             $this->em->getConnection()->rollBack();
 
             throw new TransferFailedException('Fund transfer failed', 500, previous: $e);
+        } finally {
+            $lock->release();
         }
     }
 
@@ -80,5 +88,13 @@ readonly class FundTransferService
             $transferRequest->getCurrency(),
             $targetCurrency,
         );
+    }
+
+    private function getLockKey(Account $sender, Account $recipient): string
+    {
+        $ids = [$sender->getId()->toRfc4122(), $recipient->getId()->toRfc4122()];
+        sort($ids);
+
+        return 'fund_transfer_lock_' . implode('_', $ids);
     }
 }
