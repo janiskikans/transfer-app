0dd6a5e9d6068e1af90ea8d78c988e88c96f5c93
Janis Kikans <janis.kikans@gmail.com>
2025-06-10 23:37:58 +0300

    fix

--- src/Currency/Repository/CachedCurrencyRepository.php
+++ src/Currency/Repository/CachedCurrencyRepository.php
@@ -5,14 +5,18 @@ declare(strict_types=1);
 namespace App\Currency\Repository;
 
 use App\Currency\Entity\Currency;
+use Doctrine\ORM\EntityManagerInterface;
 use Psr\Cache\InvalidArgumentException;
 use Symfony\Contracts\Cache\CacheInterface;
 use Symfony\Contracts\Cache\ItemInterface;
 
 final readonly class CachedCurrencyRepository implements CurrencyRepositoryInterface
 {
-    public function __construct(private CurrencyRepositoryInterface $innerRepository, private CacheInterface $cache)
-    {
+    public function __construct(
+        private CurrencyRepositoryInterface $innerRepository,
+        private CacheInterface $cache,
+        private EntityManagerInterface $entityManager,
+    ) {
     }
 
     /**
@@ -20,10 +24,12 @@ final readonly class CachedCurrencyRepository implements CurrencyRepositoryInter
      */
     public function getByCode(string $code): ?Currency
     {
-        return $this->cache->get('currency_' . $code, function (ItemInterface $item) use ($code): ?Currency {
+        $currency = $this->cache->get('currency_' . $code, function (ItemInterface $item) use ($code): ?Currency {
             $item->expiresAfter(12 * 60 * 60); // 12 hours
 
             return $this->innerRepository->getByCode($code);
         });
+
+        return $this->entityManager->getReference(Currency::class, $currency->getCode());
     }
 }
